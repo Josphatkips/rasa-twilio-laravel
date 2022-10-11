536a41de52f50de886ea00124bc119eab3c2753b
Josphat Kipyegon <josphatkips@gmail.com>
2022-10-11 18:49:08 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('twilio-webhook',[SiteController::class,'twilioWebHook']);
+Route::any('twilio-webhook',[SiteController::class,'twilioWebHook']);
